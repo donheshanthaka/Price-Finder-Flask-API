4b9127eb377f4df76ff8dad2937ce2182d4623d9
Don Heshanthaka <heshanthaka007@gmail.com>
2022-06-24 14:47:38 +0530

    Implementation of a basic html based flask app

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=['GET'])
 def hello_world():
-    return "Hello World"
+    return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def predict():
+    image_file = request.files['imageFile']
+    image_path = './images/' + image_file.filename
+    image_file.save(image_path)
+    
+    return render_template('index.html') 
 
-if __name__ == "__main__":
-     app.run(port=3000, debug=True)
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)

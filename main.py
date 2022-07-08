69caaf6f221902553c7738830597346a6d172aed
Don Heshanthaka <heshanthaka007@gmail.com>
2022-07-08 17:54:39 +0530

    Changed the return data of predict function to the name without probability

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-#loaded_model_1 = tf.keras.models.load_model('model/feature_extraction_efficientnetB1')
-#class_names = ['Alto 2015', 'Hero Dash 2016', 'Toyota Aqua 2014', 'Wagon R Stingray 2018']
+loaded_model_1 = tf.keras.models.load_model('model/feature_extraction_efficientnetB1')
+class_names = ['Alto 2015', 'Hero Dash 2016', 'Toyota Aqua 2014', 'Wagon R Stingray 2018']
 
 app = Flask(__name__)
 
@@ -14,24 +14,24 @@ app = Flask(__name__)
 # def hello_world():
 #     return render_template('index.html')
 
-# @app.route('/', methods=['POST'])
-# def predict():
-#     image_file = request.files['imageFile']
-#     image_type = secure_filename(image_file.filename).split('.')[1]
-#     print(image_type)
-#     if (image_type != 'jpeg' and image_type != 'png'):
-#       return 'File type not supported!'
-#     image_path = './images/' + secure_filename(image_file.filename)
-#     image_file.save(image_path)
+@app.route('/', methods=['POST'])
+def predict():
+    image_file = request.files['imageFile']
+    image_type = secure_filename(image_file.filename).split('.')[1]
+    print(image_type)
+    if (image_type != 'jpeg' and image_type != 'png'):
+      return 'File type not supported!'
+    image_path = './images/' + secure_filename(image_file.filename)
+    image_file.save(image_path)
     
-#     img = load_and_prep_image(image_path, scale=False)
-#     pred_prob = loaded_model_1.predict(tf.expand_dims(img, axis=0), verbose=0) # make prediction on image with shape [1, 224, 224, 3] (same shape as model was trained on)
-#     pred_class = class_names[pred_prob.argmax()] # get the index with the highet prediction probability
-#     classification = (f"pred: {pred_class}, prob: {pred_prob.max():.2f}")
+    img = load_and_prep_image(image_path, scale=False)
+    pred_prob = loaded_model_1.predict(tf.expand_dims(img, axis=0), verbose=0) # make prediction on image with shape [1, 224, 224, 3] (same shape as model was trained on)
+    pred_class = class_names[pred_prob.argmax()] # get the index with the highet prediction probability
+    # classification = (f"pred: {pred_class}, prob: {pred_prob.max():.2f}")
 
-#     os.remove(image_path)
-#     #return render_template('index.html', prediction = classification) 
-#     return (classification) 
+    os.remove(image_path)
+    #return render_template('index.html', prediction = classification) 
+    return (pred_class)
 
 # Create a function to load and prepare images
 def load_and_prep_image(filename, img_shape=224, scale=True):

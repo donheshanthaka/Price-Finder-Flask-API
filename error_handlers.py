cb8909f6876e9ceaa99703a47fc7940a1d84add7
Don Heshanthaka <heshanthaka007@gmail.com>
2022-07-30 23:58:46 +0530

    Moved InvalidImageType exception to error_handler.py

--- error_handlers.py
+++ error_handlers.py
@@ -1,3 +1,10 @@
 from main import app
-from flask import request
+# from flask import request
 
+class InvalidImageType(Exception):
+    pass
+
+@app.errorhandler(InvalidImageType)
+def invalid_image_type(e):
+    """Return a 415 (Unsupported Media Type) http status code with the error message (Invalid Image Type)"""
+    return {'message': 'Invalid Image Type'}, 415

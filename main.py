4a97e8e5efd7675ae0278660caac0cf87251c7a4
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-08 10:02:04 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -172,4 +172,5 @@ if __name__ == '__main__':
     # When using the android emulator
     # When using the android emulator
     # app.run(host="0.0.0.0", port=8000, debug=True)
+    # test
     app.run() # For production

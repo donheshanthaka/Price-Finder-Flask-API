a0949a686e1306a783e2b0c3434e062a0caa468d
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-09 13:14:19 +0530

    Updated main.py to run the production env

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from app import app
 
 if __name__ == '__main__':
-    # When using the android emulatorr
     # When using the android emulator
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    # app.run(host="0.0.0.0", port=8000, debug=True)
     # test
-    # app.run() # For production
+    app.run() # For production

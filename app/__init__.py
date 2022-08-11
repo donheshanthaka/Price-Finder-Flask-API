3b125680df49427e98e1bbf5e05732b130607816
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-11 21:53:18 +0530

    Implement flask app configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# app.config.from_object("config.DevelopmentConfig")
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+
+elif app.config["ENV"] == "development":
+    app.config.from_object("config.DevelopmentConfig")
+
+else:
+    app.config.from_object("config.ProductionConfig")
+
+
 from app import views

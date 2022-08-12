1e9141506d27ed68ee49b0d473be3632ad295682
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-13 04:33:24 +0530

    Implement helpers.py to handle helper functionality

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from app import error_handlers
 from app import app
+from app.helpers import load_and_prep_image
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
@@ -50,35 +51,7 @@ def predict():
 
     return (prediction)
 
-# Create a function to load and prepare images
-def load_and_prep_image(filename, img_shape=224, scale=True):
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  specified shape (img_shape, img_shape, color_channels=3).
 
-  Args:
-    filename (str): path to target image
-    image_shape (int): height/width dimension of target image size
-    scale (bool): scale pixel values from 0-255 to 0-1 or not
-  
-  Returns:
-    Image tensor of shape (img_shape, img_shape, 3)
-  """
-  # Read in the image
-  img = tf.io.read_file(filename)
-
-  # Decode image into tensor
-  img = tf.io.decode_image(img, channels=3)
-
-  # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
-
-  # Scale? Yes/no
-  if scale:
-    # rescale the image (get all values between 0 and 1)
-    return img/255.
-  else:
-    return img # don't need to rescale images for EfficientNet models in TensorFlow
 
 
 # @app.route('/price', methods=['GET'])

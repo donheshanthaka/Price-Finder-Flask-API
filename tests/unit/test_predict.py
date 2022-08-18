03b9016b06727fee13ba6e58280bfc31064af765
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-18 21:47:25 +0530

    Create test_load_and_prep_image to test the load_and_prep_image function

--- tests/unit/test_predict.py
+++ tests/unit/test_predict.py
@@ -4,7 +4,7 @@ image_path = "tests/images/1.jpeg"
 
 def test_predict():
     """
-    GIVEN an image file path of a vehicle
+    GIVEN an image file path of a vehicle (Toyota Aqua)
     WHEN trying to identify the vehicle
     THEN check the identified vehicle is correct
     """
@@ -12,3 +12,11 @@ def test_predict():
     assert vehicle == "Toyota Aqua 2014"
 
 
+def test_load_and_prep_image():
+    """
+    GIVEN an image file path
+    WHEN trying to predict the imnage
+    THEN check the returned image tensor is in correct shape
+    """
+    img = helpers.load_and_prep_image(image_path, scale=False)
+    assert img.shape == (224, 224, 3)

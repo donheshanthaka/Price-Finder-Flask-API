3b24f5d6e60ff366a12964cd05bebb9063b7d69e
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-18 23:26:01 +0530

    Create test_get_price to test the get_price function

--- tests/unit/test_predict.py
+++ tests/unit/test_predict.py
@@ -1,12 +1,13 @@
+import string
 from app import helpers
 
 image_path = "tests/images/1.jpeg"
 
 def test_predict():
     """
-    GIVEN an image file path of a vehicle (Toyota Aqua)
+    GIVEN a path to an image of a vehicle (Toyota Aqua)
     WHEN trying to identify the vehicle
-    THEN check the identified vehicle is correct
+    THEN check the identified vehicle is correct according to the given image
     """
     vehicle = helpers.predict(image_path)
     assert vehicle == "Toyota Aqua 2014"
@@ -14,9 +15,19 @@ def test_predict():
 
 def test_load_and_prep_image():
     """
-    GIVEN an image file path
+    GIVEN a path to an image
     WHEN trying to predict the imnage
     THEN check the returned image tensor is in correct shape
     """
     img = helpers.load_and_prep_image(image_path, scale=False)
     assert img.shape == (224, 224, 3)
+
+
+def test_get_price():
+    """
+    GIVEN a name of a vehicle
+    WHEN trying to find the current market price
+    THEN check the returned value is a string (cannot check for an exact value since market value is not constant, therefore checking the return type is the only option)
+    """
+    price = helpers.get_price("Toyota Aqua 2014")
+    assert type(price) == str

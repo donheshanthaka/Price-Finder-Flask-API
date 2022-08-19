ba294881fe5f93c79be0be51d25ca5d26232fc1f
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-19 06:06:35 +0530

    Create test_get_vehicle_without_image to test status code returned by ImageFileNotFound exception

--- tests/functional/test_get_vehicle_info.py
+++ tests/functional/test_get_vehicle_info.py
@@ -1,6 +1,7 @@
 from app import create_app
 
 image_path = "tests/images/1.jpeg"
+invalid_image_path = "tests/images/dash.bmp"
 
 img = open(image_path, 'rb')
 
@@ -18,3 +19,17 @@ def test_get_vehicle():
         assert response.status_code == 200
         assert b'model' in response.data
         assert b'price' in response.data
+
+
+def test_get_vehicle_without_image():
+    """
+    GIVEN a Flask application
+    WHEN the '/get-vehicle-info' is requested (POST) without an image attached in the body
+    THEN check that a '400' status code is returned
+    """
+    flask_app = create_app()
+
+    # Create a test client using the FLask application
+    with flask_app.test_client() as test_client:
+        response = test_client.post('/get-vehicle-info')
+        assert response.status_code == 400

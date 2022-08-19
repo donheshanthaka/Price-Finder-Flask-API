4c30d87954dd65637d24df275b4ca6a16c4294d7
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-19 06:03:45 +0530

    Bug Fix: Fix file file not found exception on get_vehicle function

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ def test():
   try:
     image_file = request.files['imageFile']
   except KeyError:
-    raise error_handlers.image_file_not_found
+    raise error_handlers.ImageFileNotFound
   image_type = secure_filename(image_file.filename).split('.')[1]
   if (image_type != 'jpeg' and image_type != 'png'):
     # Return a 415 (Unsupported Media Type) http status code
@@ -32,7 +32,7 @@ def get_vehicle():
     try:
       image_file = request.files['imageFile']
     except KeyError:
-      raise error_handlers.image_file_not_found
+      raise error_handlers.ImageFileNotFound
     image_type = secure_filename(image_file.filename).split('.')[1]
     if (image_type != 'jpeg' and image_type != 'png' and image_type != 'jpg'):
       # Return a 415 (Unsupported Media Type) http status code

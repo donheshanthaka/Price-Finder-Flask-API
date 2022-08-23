ea39d56e5fb2e10ac5dc7afd89d75eb728697adf
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-23 19:39:17 +0530

    Code refactoring on test_get_price.py

--- tests/unit/test_get_price.py
+++ tests/unit/test_get_price.py
@@ -4,7 +4,9 @@ def test_get_price():
     """
     GIVEN a name of a vehicle
     WHEN trying to find the current market price
-    THEN check the returned value is a string (cannot check for an exact value since market value is not constant, therefore checking the return type is the only option)
+    THEN check the returned value is a string 
+    (cannot check for an exact value since market value is not constant, 
+    therefore checking the return type is the only option available)
     """
     price = helpers.get_price("Toyota Aqua 2014")
     assert type(price) == str

0635062bbda7e90a95acb6fb273dd93c5f76f913
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-23 22:54:23 +0530

    Code refactoring on test_get_price.py

--- tests/unit/test_get_price.py
+++ tests/unit/test_get_price.py
@@ -1,11 +1,12 @@
 from app import utils
 
+
 def test_get_price():
     """
     GIVEN a name of a vehicle
     WHEN trying to find the current market price
-    THEN check the returned value is a string 
-    (cannot check for an exact value since market value is not constant, 
+    THEN check the returned value is a string
+    (cannot check for an exact value since market value is not constant,
     therefore checking the return type is the only option available)
     """
     price = utils.get_price("Toyota Aqua 2014")

da568fb5fc67c5f5da39e1f4a86b7d42ba4ad40b
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-23 19:37:24 +0530

    Code refactoring on __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,19 @@ from flask import Flask
 from app.views import views
 from app.error_handlers import error_handlers
 
-def create_app():
 
+def create_app():
+    """Returns an instance of an application object of the app"""
     app = Flask(__name__)
 
-    # app.config.from_object("config.DevelopmentConfig")
-    if app.config["ENV"] == "production":
-        app.config.from_object("config.ProductionConfig")
+    if app.config['ENV'] == 'production':
+        app.config.from_object('config.ProductionConfig')
 
-    elif app.config["ENV"] == "development":
-        app.config.from_object("config.DevelopmentConfig")
+    elif app.config['ENV'] == 'development':
+        app.config.from_object('config.DevelopmentConfig')
 
     else:
-        app.config.from_object("config.ProductionConfig")
+        app.config.from_object('config.ProductionConfig')
 
     app.register_blueprint(views)
     app.register_blueprint(error_handlers)

2eefcc1426ae7d7d346f67114f1f5d44a8bb0102
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-23 19:35:38 +0530

    Code refactoring on main.py

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # When using the android emulator
-    # app.run(host="0.0.0.0", port=8000)
-    app.run() # For production
+    app.run()

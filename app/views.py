3a48812480dcf3457aa470d87d552b375a5c326d
Don Heshanthaka <heshanthaka007@gmail.com>
2022-08-23 19:37:54 +0530

    Code refactoring on views.py

--- app/views.py
+++ app/views.py
@@ -1,13 +1,11 @@
-# from fileinput import filename
 from flask import jsonify, request, Blueprint
 from werkzeug.utils import secure_filename
 import os
 import uuid
-
 from app import error_handlers
 from app.helpers import predict, get_price
 
-views = Blueprint("views", __name__)
+views = Blueprint('views', __name__)
 
 # @views.route('/test', methods=['GET', 'POST'])
 # def test():
@@ -24,33 +22,37 @@ views = Blueprint("views", __name__)
 #   data = {"model": "Wagon R", "price": "Rs. 6,500,000"}
 #   return jsonify(data)
 
-
-
 @views.route('/get-vehicle-info', methods=['POST'])
 def get_vehicle():
-    prediction = {}
+    """
+    Reads and save the image recieved through the POST request and
+    pass it to the predict function to identify the vehicle and then
+    use utility function get_price to retrieve the market price.
+
+    Returns:
+      Json object of the vehicle model and price
+    """
+    vehicle_info = {}
     try:
       image_file = request.files['imageFile']
     except KeyError:
       raise error_handlers.ImageFileNotFound
     image_type = secure_filename(image_file.filename).split('.')[1]
+
     if (image_type != 'jpeg' and image_type != 'png' and image_type != 'jpg'):
       # Return a 415 (Unsupported Media Type) http status code
       raise error_handlers.InvalidImageType
+    
     # Create a unique id for the filename
     uid = str(uuid.uuid4())    
     filename = uid + secure_filename(image_file.filename)
     image_path = './images/' + filename
     image_file.save(image_path)
-
     vehicle = predict(image_path)
-
-    prediction['model'] = vehicle
-
+    vehicle_info['model'] = vehicle
     os.remove(image_path)
+    vehicle_info['price'] = get_price(vehicle)
 
-    prediction['price'] = get_price(vehicle)
-
-    return (prediction)
+    return (vehicle_info)
 
 

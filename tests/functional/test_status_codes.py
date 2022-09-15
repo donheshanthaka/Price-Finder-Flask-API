216d93660e4f042f4458c1f58db3f1d513018af6
Don Heshanthaka <heshanthaka007@gmail.com>
2022-09-15 23:40:32 +0530

    Implement functional test case test_method_not_allowed

--- tests/functional/test_status_codes.py
+++ tests/functional/test_status_codes.py
@@ -2,10 +2,22 @@ def test_page_not_found(client, image_path):
     """
     GIVEN a Flask application
     WHEN an invalid URL / Endpoint is requested (POST)
-    THEN check that a '404 Not Found' status code is returned 
+    THEN check that a '404 Not Found' status code is returned
     """
     response = client.post('/invalid-path',
                            data={'imageFile': open(image_path, 'rb')})
 
     assert response.status_code == 404
+    assert b'message' in response.data
+
+
+def test_method_not_allowed(client):
+    """
+    GIVEN a Flask application
+    WHEN an invalid request is made to a valid endpoint (GET)
+    THEN check that a '405 Method Not Allowed' status code is returned
+    """
+    response = client.get('/get-vehicle-info')
+
+    assert response.status_code == 405
     assert b'message' in response.data
